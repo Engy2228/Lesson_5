5690c3fadf436f1eed9e46291ede0d65c5efd113
Engy2228 <109961605+Engy2228@users.noreply.github.com>
2024-10-01 18:41:04 +0300

    Add files via upload

---
+++ const.js
@@ -0,0 +1,17 @@
+const question1 = 'Какая самая маленькая страна мира по населению?'
+const question2 = 'Какая самая большая страна мира по населению?'
+const question3 = 'Какой самый большой материк на планете?'
+/*
+const COLOR_ORANGE = "#FF7F00";
+
+// ...когда нам нужно выбрать цвет
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+*/
+const answer1 = 'Китай'
+const answer2 = 'Евразия'
+const answer3 = 'Ватикан'
+
+console.log(question1, 'Ответ:', answer3)
+console.log(question2, 'Ответ:', answer1)
+console.log(question3, 'Ответ:', answer2)
